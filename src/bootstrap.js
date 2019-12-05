c8ca4dffe0dbbbd3187d5fa303290ec1936cac69
Lucas Mauro <lucas_de_mauro@hotmail.com>
2019-12-05 16:05:11 -0300

    Teste de criacao de usuario

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,3 +1,7 @@
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
-dotenv.config({ path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env' });
+// dotenv.config({ path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env' });
+
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+});
