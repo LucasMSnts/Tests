c67c1d8e8f6eac3ffc183aa1cdf59825845c5612
Lucas Mauro <lucas_de_mauro@hotmail.com>
2019-12-06 15:53:37 -0300

    [fix] folder

--- src/bootstrap.js
+++
@@ -1,7 +0,0 @@
-// import dotenv from 'dotenv';
-
-// dotenv.config({ path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env' });
-
-require('dotenv').config({
-  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
-});
