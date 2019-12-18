a2f038c34966fd8879312d5b531a449fd786b177
Lucas Mauro <lucas_de_mauro@hotmail.com>
2019-12-18 15:15:05 -0300

    Testando formulario

--- web/src/__tests__/components/TechList.test.js
+++ web/src/__tests__/components/TechList.test.js
@@ -5,14 +5,14 @@ import TechList from '~/components/TechList';
 
 describe('Tech List component', () => {
     it('should be able to add new tech', () => {
-        const { getByText, getByTestId, debug } = render(<TechList />);
+        const { getByText, getByTestId, getByLabelText } = render(<TechList />);
 
-        debug(); // ver como fica a pagina (html)
+        //debug(); // ver como fica a pagina (html)
 
-        fireEvent.click(getByText('Adicionar'));
-
-        debug();
+        fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' } });
+        fireEvent.submit(getByTestId('tech-form'));
 
         expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+        expect(getByLabelText('Tech')).toHaveValue('');
     });
 });
